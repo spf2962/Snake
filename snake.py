df22932bf1adb932bb7586b8b75472c90e3ca70e
Stephen Farrell <43182523+spf2962@users.noreply.github.com>
2019-09-22 22:10:37 -0400

    Implements the move method, instead of crashing into the wall ive made it pop to the other side when the snake reaches the boundaries
    Signed-off-by: Stephen Farrell <43182523+spf2962@users.noreply.github.com>

--- snake.py
+++ snake.py
@@ -34,9 +34,57 @@ class snake(object):
         self.color = color
         self.head = cube(pos)
         self.body.append(self.head)
+        self.dirnx = 0
+        self.dirny = 1
+
 
     def move(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+            keys = pygame.key.get_pressed()
+
+            for key in keys:
+                if keys[pygame.K_LEFT]:
+                    self.dirnx = -1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+                elif keys[pygame.K_RIGHT]:
+                    self.dirnx = 1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+                elif keys[pygame.K_UP]:
+                    self.dirnx = 0
+                    self.dirny = -1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+                elif keys[pygame.K_DOWN]:
+                    self.dirnx = 0
+                    self.dirny = 1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+            for index, cub in enumerate(self.body):
+                position = cub.pos[:]
+                if position in self.turns:
+                    turn = self.turns[position]
+                    cub.move(turn[0], turn[1])
+                    if index == len(self.body) - 1:
+                        self.turns.pop(position)
+                # Check if the snake goes out of bounds
+                else:
+                    # If snake reaches the left side of the screen move it to the right side
+                    if cub.dirnx == -1 and cub.pos[0] <= 0: cub.pos = (cub.rows-1, cub.pos[1])
+                    # If snake reaches the right side of the screen move it to the left side of the screen
+                    elif cub.dirnx == 1 and cub.pos[0] >= cub.rows-1: cub.pos = (0, cub.pos[1])
+                    # If snake reaches the top of the screen move it to the bottom row
+                    elif cub.dirny == 1 and cub.pos[1] >= cub.rows-1: cub.pos = (cub.pos[0], 0)
+                    # If snake reaches the bottom of the screen move it to the top row
+                    elif cub.dirny == -1 and cub.pos[1] <= 0: cub.pos = (cub.pos[0], cub.rows-1)
+                    else: cub.move(cub.dirnx, cub.dirny)
+
 
     def reset(self, pos):
         pass
